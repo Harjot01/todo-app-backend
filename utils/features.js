59fff3208b654e569d9952dc08e346211bd76704
Harjot01 <harjotsingh.5125@gmail.com>
2024-10-16 15:41:11 +0530

    Fixed the production env

--- utils/features.js
+++ utils/features.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const sendCookie = (user, res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
   return res
     .status(statusCode)
     .cookie("token", token, {
@@ -10,6 +9,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
       maxAge: 24 * 15 * 60 * 1000,
       sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Development" ? false : true,
+      
     })
     .json({
       success: true,
