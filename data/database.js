a165fd4f1b0b39fd5a0e0a7cdefbfb7444843fb8
Harjot01 <harjotsingh.5125@gmail.com>
2024-10-16 13:00:29 +0530

    Updated Database

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(`${process.env.MONGO_URI}/todo-backend`)
-    .then(console.log("Database Connected"))
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Database Connected"))
     .catch((e) => console.log(e));
 };
