747f1f680a590483e39dfa2d6259ee8e687e34f4
Harjot01 <harjotsingh.5125@gmail.com>
2024-10-17 22:35:09 +0530

    Updated logout function

--- controllers/user.js
+++ controllers/user.js
@@ -57,7 +57,8 @@ export const Logout = (req, res) => {
   res
     .status(200)
     .cookie("token", "", {
-      expires: new Date(Date.now()),
+      expires: new Date(0),
+      httpOnly: true,
       sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Development" ? false : true,
     })
